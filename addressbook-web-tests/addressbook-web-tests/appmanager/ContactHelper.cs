77668480dd6ea793a0e03b696f6d3ee24d5e0280
Анастасия Памбукян <pumbum26@gmail.com>
2017-04-23 23:31:01 +0300

    3/1

--- addressbook-web-tests/addressbook-web-tests/appmanager/ContactHelper.cs
+++ addressbook-web-tests/addressbook-web-tests/appmanager/ContactHelper.cs
@@ -60,12 +60,10 @@ namespace WebAddressBookTests
 
         public void FillContactForm(ContactData contact)
         {
-            driver.FindElement(By.Name("firstname")).Clear();
-            driver.FindElement(By.Name("firstname")).SendKeys(contact.Firstname);
-            driver.FindElement(By.Name("middlename")).Clear();
-            driver.FindElement(By.Name("middlename")).SendKeys(contact.Middlename);
-            driver.FindElement(By.Name("lastname")).Clear();
-            driver.FindElement(By.Name("lastname")).SendKeys(contact.Lastname);
+            Type(By.Name("firstname"), contact.Firstname);
+            Type(By.Name("middlename"), contact.Middlename);
+            Type(By.Name("lastname"), contact.Lastname);
+
         }
 
         public ContactHelper SubmitContactModification()
